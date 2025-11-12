98c32773b3477ce2781dc29cb30018fea1e7e6ae
rdyagelev <topor13@mail.ru>
2025-11-12 14:24:29 +0300

    homework04: some fixes for SFINAE use

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <list>
 #include <tuple>
 
-template<typename T, typename = std::enable_if<std::is_integral<T>::value>>
-void print_ip(T v) {
+//SFINAE for integrals
+template<typename T>
+auto print_ip(T v) -> std::enable_if_t<std::is_integral<T>::value> {
     const size_t sz = sizeof(T);
     const unsigned char* bytes = reinterpret_cast<const unsigned char*>(&v);
     for (size_t i = (sz - 1); i > 0; --i)
@@ -16,28 +17,21 @@ void print_ip(T v) {
     std::cout << static_cast<int>(bytes[0]) << std::endl;
 } 
 
-template<>
-void print_ip(std::string v) {
+// SFINAE for std::string without dots(std::string also container)
+template<typename T>
+auto print_ip(T v) -> std::enable_if_t<
+        std::is_same_v<decltype(v), std::string>,
+    void> {
     std::cout << v << std::endl;
 }
 
-template<>
-void print_ip(std::vector<int> v) {
-    if (v.size() > 1) {
-        for (auto it = v.begin(); it < (v.end() - 1); ++it)
-        {
-            std::cout << *it << ".";
-        }
-        std::cout << *(v.end() - 1) << std::endl;
-    } else if (v.size() == 1) {
-        std::cout << *v.begin() << std::endl;
-    } else {
-        std::cout << "empty vector!" << std::endl;
-    }
-}
-
-template<>
-void print_ip(std::list<short> v) {
+// SFINAE for any container that have begin() method
+// for list and vector containers
+template<typename T>
+auto print_ip(T v) -> std::enable_if_t<
+        std::is_same_v<decltype(v.begin()), typename T::iterator> &&
+        !std::is_same_v<decltype(v), std::string>,
+    void> {
     if (v.size() > 1) {
         for (auto it = v.begin(); it != (--v.end()); ++it)
         {
@@ -47,7 +41,7 @@ void print_ip(std::list<short> v) {
     } else if (v.size() == 1) {
         std::cout << *v.begin() << std::endl;
     } else {
-        std::cout << "empty list!" << std::endl;
+        std::cout << "empty container!" << std::endl;
     }
 }
 
@@ -58,17 +52,12 @@ void print_tuple(const T& t, std::index_sequence<idx...>) {
     std::cout << std::endl;
 }
 
+// Only for tuple reload
 template<typename... T>
-void print_ip(std::tuple<T...> v) {
-    /*
-    size_t count = sizeof...(T);
-    for (size_t i = 0; i < (count - 1); i++)
-    {
-        print_tuple_element(v, i);
-    }*/
-    if constexpr (std::conjunction_v<std::is_same<T, typename std::common_type_t<T...>>...>) {
-        print_tuple(v, std::index_sequence_for<T...>{});
-    }
+auto print_ip(std::tuple<T...> v) -> std::enable_if_t<
+        std::is_same_v<decltype(v), std::tuple<T...>>,
+    void> {
+    print_tuple(v, std::index_sequence_for<T...>{});
 }
 
 /**
@@ -77,22 +66,23 @@ void print_ip(std::tuple<T...> v) {
 	\version 0.0.1
 	\date 07.11.2025
 
-	Main function for print_ip programm. This programm make for studying on otuscpp course and print ip
-    with special print_ip() method, reloaded in templates.
-    It can be integral digit, container(vector or list), std::string or tuple.
-    Examples:
-        print_ip( int8_t{-1} ); // 255 
-        print_ip( int16_t{0} ); // 0.0
-        print_ip( int32_t{2130706433} ); // 127.0.0.1 
-        print_ip( int64_t{8875824491850138409} ); // 123.45.67.89.101.112.131.41 
-        print_ip( std::string{"Hello, World!"} ); // Hello, World! 
-        print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400 
-        print_ip( std::vector<int>{100} );
-        print_ip( std::vector<int>{} );
-        print_ip( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100 
-        print_ip( std::list<short>{400} );
-        print_ip( std::list<short>{} );
-        print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+	Main function for print_ip programm. \n
+    This programm make for studying on otuscpp course and print ip \n
+    with special print_ip() method, reloaded in templates. \n
+    It can be integral digit, container(vector or list), std::string or tuple. \n
+    Examples:\n
+        print_ip( int8_t{-1} ); // 255 \n
+        print_ip( int16_t{0} ); // 0.0 \n
+        print_ip( int32_t{2130706433} ); // 127.0.0.1 \n
+        print_ip( int64_t{8875824491850138409} ); // 123.45.67.89.101.112.131.41 \n
+        print_ip( std::string{"Hello, World!"} ); // Hello, World! \n
+        print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400 \n
+        print_ip( std::vector<int>{100} ); \n
+        print_ip( std::vector<int>{} ); \n
+        print_ip( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100 \n
+        print_ip( std::list<short>{400} ); \n
+        print_ip( std::list<short>{} ); \n
+        print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0 \n
 */
 int main()
 {
